185b5e700251c25357c3d052600aa865d6286aac
Eduardo Escárez <eduardoe@gmail.com>
2023-02-07 18:22:49 -0300

    Sección 25

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,35 @@
 <script setup>
 const name = "Vue dinámico"
-const arrayColores = ["blue", "red", "peru"]
-const activo = false
-const arrayEmojiFrutas = ["🍎", "🍌", "🍉", "🍓", "🍒"]
 const arrayFrutas = [
-        {
-            name: "Manzana",
-            price: "$1.00",
-            description: "Una manzana",
-        },
-        {
-            name: "Pera",
-            price: "$2.00",
-            description: "Una pera",
-        },
-        {
-            name: "Naranja",
-            price: "$3.00",
-            description: "Una naranja",
-        },
-    ];
-const objetoFruta = {
-    name: "Naranja",
-    price: "$3.00",
-    description: "Una naranja",
-};
+    {
+        name: "Manzana",
+        price: "$1.00",
+        description: "Una manzana",
+        stock: 0,
+    },
+    {
+        name: "Pera",
+        price: "$2.00",
+        description: "Una pera",
+        stock: 10,
+    },
+    {
+        name: "Naranja",
+        price: "$3.00",
+        description: "Una naranja",
+        stock: 20,
+    },
+];
 
 </script>
 
 <template>
     <h1>Hola {{ name.toUpperCase() }}</h1>
-    <h2>{{ arrayColores }}</h2>
-    <h2 :style="`color: ${arrayColores[2]}`">Soy azul</h2>
-    <p v-if="activo === true">Estoy activo</p>
-    <p v-else-if="activo === false">Estoy inactivo</p>
-    <p v-else>Estoy indeciso</p>
-    <p v-show="activo">Estoy activo v-show</p>
 
     <ul>
-        <li v-for="(fruta, index) in arrayEmojiFrutas" :key="index">{{ index }} - {{ fruta }}</li>
-    </ul>
-
-    <br/>
-
-    <ul>
-        <li v-for="(fruta) in arrayFrutas" :key="fruta.name">{{ fruta.name }} - {{ fruta.price }} - {{ fruta.description }}</li>
-    </ul>
-
-    <br/>
-
-    <ul>
-        <li v-for="(value, propiedad, index) in objetoFruta" :key="value">{{ propiedad }} : {{ value }} - {{ index }}</li>
+        <li v-for="item in arrayFrutas" :key="item.name">
+            {{ item.name }} - {{ item.price }} - {{ item.description }} - {{ item.stock }}
+        </li>
     </ul>
 
 
