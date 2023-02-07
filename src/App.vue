51ced06342aeee7e3cfb809a964571f24ebe8a3e
Eduardo Escárez <eduardoe@gmail.com>
2023-02-07 18:36:37 -0300

    Seccion 27

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,17 @@
 <script setup>
-const name = "Vue dinámico"
-const arrayFrutas = [
-    {
-        name: "Manzana",
-        price: "$1.00",
-        description: "Una manzana",
-        stock: 0,
-    },
-    {
-        name: "Pera",
-        price: "$2.00",
-        description: "Una pera",
-        stock: 10,
-    },
-    {
-        name: "Naranja",
-        price: "$3.00",
-        description: "Una naranja",
-        stock: 20,
-    },
-];
+    const name = "Vue dinámico"
+
+// metodo
+    const cliqueador = () =>{
+        console.log ("Un click recibido 😅")
+    }
 
 </script>
 
 <template>
     <h1>Hola {{ name.toUpperCase() }}</h1>
 
-    <ul>
-        <template v-for="item in arrayFrutas" :key="item.name">
-            <li v-if="item.stock > 0">{{ item.name }} - {{ item.price }} - {{ item.description }} - {{ item.stock }}</li>
-        </template>
-    </ul>
+    <button v-on:click="cliqueador">Activame</button>
 
 
 </template>
