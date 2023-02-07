8cf82fe78210ff9aa804f636e38efb31ccbb564f
Eduardo Escárez <eduardoe@gmail.com>
2023-02-07 01:01:20 -0300

    Cambios hasta modulo 22 de curso

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,29 @@
+<script setup>
+const name = "Vue dinámico"
+const styleColor = "color: blue;"
+const arrayColores = ["blue", "red", "peru"]
+const activo = false
+const arrayFrutas = ["🍎", "🍌", "🍉", "🍓", "🍒"]
+</script>
+
 <template>
-    <h1>Hola vuejs</h1>
+    <h1>Hola {{ name.toUpperCase() }}</h1>
+    <h2>{{ arrayColores }}</h2>
+    <h2 :style="`color: ${arrayColores[2]}`">Soy azul</h2>
+    <p v-if="activo === true">Estoy activo</p>
+    <p v-else-if="activo === false">Estoy inactivo</p>
+    <p v-else>Estoy indeciso</p>
+    <p v-show="activo">Estoy activo v-show</p>
+
+    <ul>
+        <li v-for="(fruta, index) in arrayFrutas" :key="index">{{ index }} - {{ fruta }}</li>
+    </ul>
+
+    <br/>
 </template>
+
+<style>
+h1{
+    color: red;
+}
+</style>
